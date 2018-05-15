30d667bd861f50cfbb9fedf1196604b46b3a3f4f
SethJohnsonPDX <sethjohnsonpdx@gmail.com>
2018-05-15 00:21:13 -0700

    commit all

--- datetime66/Program.cs
+++ datetime66/Program.cs
@@ -10,6 +10,14 @@ namespace datetime66
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number: ");
+            double input = Convert.ToDouble(Console.ReadLine());
+
+            DateTime hoursFromEntry = DateTime.Now.AddHours(input);
+            Console.WriteLine(hoursFromEntry);
+            Console.ReadLine();
+
         }
     }
 }
